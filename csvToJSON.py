c53e60b34899f4c693919852665aa37c7fab5917
Sarah Guermond <s.guermond@gmail.com>
2015-02-26 17:03:37 -0800

    Added reference files, edited path

--- csvToJSON.py
+++ csvToJSON.py
@@ -3,6 +3,7 @@
 
 import io
 import pprint
+import json
 
 def getValues(fileName):
 	#read in 2-2 loop
@@ -30,20 +31,18 @@ def getValues(fileName):
 	return colsToRowsToValues
 
 
-loop_2_2 = getValues('/Users/sarahguermond/Files/Loop_2_2.csv')
-loop_1_2 = getValues('/Users/sarahguermond/Files/Loop_1_2.csv')
-bulge_1nt = getValues('/Users/sarahguermond/Files/Bulge_1nt.csv')
-bulge_2nt = getValues('/Users/sarahguermond/Files/Bulge_2nt.csv')
-bulge_3nt = getValues('/Users/sarahguermond/Files/Bulge_3nt.csv')
-bulge_4nt = getValues('/Users/sarahguermond/Files/Bulge_4nt.csv')
-bulge_5nt = getValues('/Users/sarahguermond/Files/Bulge_5nt.csv')
-bulge_6nt = getValues('/Users/sarahguermond/Files/Bulge_6nt.csv')
-hairpin = getValues('/Users/sarahguermond/Files/Hairpin.csv')
-loop_1_1_GG = getValues('/Users/sarahguermond/Files/Loop_1_1_GG.csv')
-loop_1_1_UU = getValues('/Users/sarahguermond/Files/Loop_1_1_UU.csv')
-loop_1_1_noboost = getValues('/Users/sarahguermond/Files/Loop_1_1.csv')
-
-pprint.pprint(hairpin)
+loop_2_2 = getValues('Loop_2_2.csv')
+loop_1_2 = getValues('Loop_1_2.csv')
+bulge_1nt = getValues('Bulge_1nt.csv')
+bulge_2nt = getValues('Bulge_2nt.csv')
+bulge_3nt = getValues('Bulge_3nt.csv')
+bulge_4nt = getValues('Bulge_4nt.csv')
+bulge_5nt = getValues('Bulge_5nt.csv')
+bulge_6nt = getValues('Bulge_6nt.csv')
+hairpin = getValues('Hairpin.csv')
+loop_1_1_GG = getValues('Loop_1_1_GG.csv')
+loop_1_1_UU = getValues('Loop_1_1_UU.csv')
+loop_1_1_noboost = getValues('Loop_1_1.csv')
 
 # TO DO:
 # actually put this stuff into JSON and link to front end
